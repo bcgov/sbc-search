06d172e346984929540faaf0e0ab2382f1014134
Denis Calixto <deniscalixto@gmail.com>
2020-01-29 23:17:18 -0800

    Search route with pagination (another parameter in the url). 20 items per page

--- search-api/app.py
+++ search-api/app.py
@@ -1,32 +1,42 @@
 from flask import Flask, jsonify
-from models import Corporation, app
+from models import Corporation, CorpParty, app
 
 
 @app.route('/')
 def hello():
-    return "Welcome to the director search API."
+    return "Welcome to the director search API.s"
 
 
-@app.route('/search/<query>')
-def search(query):
-    results = Corporation.query.filter_by(CORP_NUM=query)
+@app.route('/search/<query>/<page>/')
+def search(query, page):
+    results = Corporation.query\
+        .join(CorpParty, Corporation.CORP_NUM == CorpParty.CORP_NUM).paginate(int(page), 20, False)
     return jsonify({
-        'results': [row.as_dict() for row in results]
+        'results': [row.as_dict() for row in results.items]
     })
+    # results = Corporation.query.filter_by(CORP_NUM=query)
+    # return jsonify({
+    #     'results': [row.as_dict() for row in results]
+    # })
+
 
 @app.route('/person/<id>')
 def person(id):
     results = CorpParty.query.filter_by(CORP_PARTY_ID=int(id))
-    return jsonify({
-        'results': results[0].as_dict()
-    })
+    if results.count() > 0:
+        print(str(results[0].CORP_PARTY_ID))
+        return jsonify(results[0].as_dict())
+    return {}
+
 
 @app.route('/corporation/<id>')
 def corporation(id):
     results = Corporation.query.filter_by(CORP_NUM=id)
-    return jsonify({
-        'results': results[0].as_dict()
-    })
+    if results.count() > 0:
+        print(str(results[0]))
+        return jsonify(results[0].as_dict())
+    return {}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
