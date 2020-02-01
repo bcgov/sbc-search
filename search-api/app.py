133c7968946480bd77b2a8a755ee12a57bcf8c63
Denis Calixto <deniscalixto@gmail.com>
2020-01-31 23:06:05 -0800

    Urls routes changes. Separate search routes for Corporation and CorpParty. CorpParty detail route fixed.

--- search-api/app.py
+++ search-api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from models import Corporation, CorpParty, app
 
 
@@ -7,26 +7,55 @@ def hello():
     return "Welcome to the director search API.s"
 
 
-@app.route('/search/<query>/<page>/')
-def search(query, page):
+@app.route('/corporation/search/')
+def corporation_search():
+
+    args = request.args
+
+    page = 1
+    if "page" in args:
+        page = int(args.get("page"))
+
+    if "query" not in args:
+        return "No search query was received", 400
+    query = args["query"]
+
     results = Corporation.query\
         .join(CorpParty, Corporation.CORP_NUM == CorpParty.CORP_NUM)\
             .filter((Corporation.CORP_NUM == query) | (CorpParty.FIRST_NME.contains(query)) | (CorpParty.LAST_NME.contains(query))) \
             .paginate(int(page), 20, False)
+            
     return jsonify({
         'results': [row.as_dict() for row in results.items]
     })
-    # results = Corporation.query.filter_by(CORP_NUM=query)
-    # return jsonify({
-    #     'results': [row.as_dict() for row in results]
-    # })
+
+
+@app.route('/person/search/')
+def corpparty_search():
+
+    args = request.args
+
+    page = 1
+    if "page" in args:
+        page = int(args.get("page"))
+
+    if "query" not in args:
+        return "No search query was received", 400
+    query = args["query"]
+
+    results = CorpParty.query\
+            .filter((CorpParty.FIRST_NME.contains(query)) | (CorpParty.LAST_NME.contains(query))) \
+            .paginate(int(page), 20, False)
+    
+    return jsonify({
+        'results': [row.as_dict() for row in results.items]
+    })
 
 
 @app.route('/person/<id>')
 def person(id):
     results = CorpParty.query.filter_by(CORP_PARTY_ID=int(id))
     if results.count() > 0:
-        print(str(results[0].CORP_PARTY_ID))
         return jsonify(results[0].as_dict())
     return {}
 
@@ -35,7 +64,6 @@ def person(id):
 def corporation(id):
     results = Corporation.query.filter_by(CORP_NUM=id)
     if results.count() > 0:
-        print(str(results[0]))
         return jsonify(results[0].as_dict())
     return {}
 
