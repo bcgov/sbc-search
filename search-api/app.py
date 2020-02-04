3686294dec522599a34557c28ada69f91c4caee6
Denis Calixto <deniscalixto@gmail.com>
2020-02-04 01:05:24 -0800

    Person list API bringing Corporation name and person address

--- search-api/app.py
+++ search-api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from models import Corporation, CorpParty, app
+from models import Corporation, CorpParty, CorpName, Address, app
 
 
 @app.route('/')
@@ -44,12 +44,47 @@ def corpparty_search():
     query = args["query"]
 
     results = CorpParty.query\
+            .join(Corporation, Corporation.CORP_NUM == CorpParty.CORP_NUM)\
+            .join(CorpName, Corporation.CORP_NUM == CorpName.CORP_NUM)\
+            .join(Address, CorpParty.MAILING_ADDR_ID == Address.ADDR_ID)\
+            .add_columns(\
+                CorpParty.CORP_PARTY_ID, 
+                CorpParty.FIRST_NME, 
+                CorpParty.MIDDLE_NME,
+                CorpParty.LAST_NME,
+                CorpParty.APPOINTMENT_DT,
+                CorpParty.CESSATION_DT,
+                Corporation.CORP_NUM,
+                CorpName.CORP_NME,
+                Address.ADDR_LINE_1,
+            )\
             .filter((CorpParty.FIRST_NME.contains(query)) | (CorpParty.LAST_NME.contains(query))) \
             .paginate(int(page), 20, False)
+
+    corp_parties = []
+    for row in results.items:
+        result_dict = {}
+
+        result_dict['CORP_PARTY_ID'] = row[1]
+        result_dict['FIRST_NME'] = row[2]
+        result_dict['MIDDLE_NME'] = row[3]
+        result_dict['LAST_NME'] = row[4]
+        result_dict['APPOINTMENT_DT'] = row[5]
+        result_dict['CESSATION_DT'] = row[6]
+        result_dict['CORP_NUM'] = row[7]
+        result_dict['CORP_NME'] = row[8]
+        result_dict['ADDR_LINE_1'] = row[9]
+
+        # print(result_dict)
+        corp_parties.append(result_dict)
+
+    # print(corp_parties)
     
-    return jsonify({
-        'results': [row.as_dict() for row in results.items]
-    })
+    return jsonify({'results': corp_parties})
+
+    # return jsonify({
+    #     'results': [row.as_dict() for row in results.items]
+    # })
 
 
 @app.route('/person/<id>')
