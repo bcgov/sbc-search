f0a8c91203095274cd45f8d9471ccf2db7477ed4
Aaron Ahn <aaron@countable.ca>
2020-02-03 23:49:51 -0800

    results page

---
+++ frontend/src/components/Search/Results.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :items-per-page="10"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          surname: "Black",
+          middlename: "Mary",
+          firstname: "Barbara",
+          appointed: "2019-30-31",
+          ceased: null,
+          acchistory: "Active",
+          org: "0559302",
+          companyname: "The Word Garden",
+          companyaddress: "#123 Sesame St..."
+        },
+        {
+          surname: "Black",
+          middlename: "Mary",
+          firstname: "Barbara",
+          appointed: "2019-30-31",
+          ceased: null,
+          acchistory: "Active",
+          org: "0559302",
+          companyname: "The Word Garden",
+          companyaddress: "#123 Sesame St..."
+        },
+        {
+          surname: "Black",
+          middlename: "Mary",
+          firstname: "Barbara",
+          appointed: "2019-30-31",
+          ceased: null,
+          acchistory: "Active",
+          org: "0559302",
+          companyname: "The Word Garden",
+          companyaddress: "#123 Sesame St..."
+        },
+        {
+          surname: "Black",
+          middlename: "Mary",
+          firstname: "Barbara",
+          appointed: "2019-30-31",
+          ceased: null,
+          acchistory: "Active",
+          org: "0559302",
+          companyname: "The Word Garden",
+          companyaddress: "#123 Sesame St..."
+        },
+        {
+          surname: "Black",
+          middlename: "Mary",
+          firstname: "Barbara",
+          appointed: "2019-30-31",
+          ceased: null,
+          acchistory: "Active",
+          org: "0559302",
+          companyname: "The Word Garden",
+          companyaddress: "#123 Sesame St..."
+        },
+        {
+          surname: "Black",
+          middlename: "Mary",
+          firstname: "Barbara",
+          appointed: "2019-30-31",
+          ceased: null,
+          acchistory: "Active",
+          org: "0559302",
+          companyname: "The Word Garden",
+          companyaddress: "#123 Sesame St..."
+        }
+      ],
+      headers: [
+        {
+          text: "Surname",
+          value: "surname",
+          sortable: true,
+          align: "left"
+        },
+        {
+          text: "Middle Name",
+          value: "middlename",
+          sortable: true,
+          align: "left"
+        },
+        {
+          text: "First Name",
+          value: "firstname",
+          sortable: false,
+          align: "left"
+        },
+        {
+          text: "Appointed",
+          value: "appointed",
+          sortable: false,
+          align: "left"
+        },
+        {
+          text: "Ceased",
+          value: "ceased",
+          sortable: false,
+          align: "left"
+        },
+        {
+          text: "Act/Hist",
+          value: "acchistory",
+          sortable: false,
+          align: "left"
+        },
+        {
+          text: "Inc/Org #",
+          value: "org",
+          sortable: false,
+          align: "left"
+        },
+        {
+          text: "Company Name",
+          value: "companyname",
+          sortable: false,
+          align: "left"
+        },
+        {
+          text: "Company Address",
+          value: "companyaddress",
+          sortable: false,
+          align: "left"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
---
+++ frontend/src/views/Results.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <section class="sbc-section">
+      <h1 class="display-2 mb-6 font-weight-bold">
+        CorpParty Search - Results
+      </h1>
+      <div class="body-1">Date: 19/09/03</div>
+      <div class="body-1">Time: 13:51:44</div>
+    </section>
+    <section class="sbc-section bg-lavender pt-12">
+      <h2 class="display-1 text-center mb-10">
+        Search Results
+      </h2>
+      <Results></Results>
+    </section>
+  </div>
+</template>
+
+<script>
+import Results from "@/components/Search/Results.vue";
+export default {
+  components: {
+    Results
+  }
+};
+</script>
+
+<style></style>
