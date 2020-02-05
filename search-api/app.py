5f09cdf665e44c2cab7b5fcda7a96c9fe8901405
Your Name <you@example.com>
2020-02-04 23:05:46 -0800

    dynamic querying

--- search-api/app.py
+++ search-api/app.py
@@ -1,11 +1,25 @@
 from flask import Flask, request, jsonify
 from models import Corporation, CorpParty, CorpName, Address, app
-
+from functools import reduce
 
 @app.route('/')
 def hello():
     return "Welcome to the director search API.s"
 
+
+def _get_filter(field, operator, value):
+    print(field, operator, value)
+    Field = getattr(CorpParty, field)
+    if operator == 'contains':
+        return Field.contains(value)
+    elif operator == 'exact':
+        return Field == value
+    elif operator == 'endswith':
+        return Field.endswith(value)
+    elif operator == 'startswith':
+        return Field.startswith(value)
+    else:
+        raise Exception('invalid operator: {}'.format(operator))
 
 @app.route('/corporation/search/')
 def corporation_search():
@@ -18,13 +32,22 @@ def corporation_search():
 
     if "query" not in args:
         return "No search query was received", 400
+
     query = args["query"]
 
     results = Corporation.query\
         .join(CorpParty, Corporation.CORP_NUM == CorpParty.CORP_NUM)\
-            .filter((Corporation.CORP_NUM == query) | (CorpParty.FIRST_NME.contains(query)) | (CorpParty.LAST_NME.contains(query))) \
-            .paginate(int(page), 20, False)
-            
+        .join(CorpName, Corporation.CORP_NUM == CorpName.CORP_NUM)
+    
+    results = results.filter(
+        (Corporation.CORP_NUM == query) |
+        (CorpName.CORP_NME.contains(query)) |
+        (CorpParty.FIRST_NME.contains(query)) |
+        (CorpParty.LAST_NME.contains(query)))
+    
+    results = results.paginate(int(page), 20, False)
+    
+    # TODO: include corpname and corpparties in serialized child using Marshmallow
     return jsonify({
         'results': [row.as_dict() for row in results.items]
     })
@@ -39,9 +62,20 @@ def corpparty_search():
     if "page" in args:
         page = int(args.get("page"))
 
-    if "query" not in args:
-        return "No search query was received", 400
-    query = args["query"]
+
+    query = args.get("query")
+
+    fields = args.getlist('field')
+    operators = args.getlist('operator')
+    values = args.getlist('value')
+
+    if len(fields) != len(operators) or len(operators) != len(values):
+        raise Exception("mismatched query param lengths: fields:{} operators:{} values:{}".format(
+            len(fields),
+            len(operators),
+            len(values)))
+
+    grps = list(zip(fields, operators, values))
 
     results = CorpParty.query\
             .join(Corporation, Corporation.CORP_NUM == CorpParty.CORP_NUM)\
@@ -57,9 +91,20 @@ def corpparty_search():
                 Corporation.CORP_NUM,
                 CorpName.CORP_NME,
                 Address.ADDR_LINE_1,
-            )\
-            .filter((CorpParty.FIRST_NME.contains(query)) | (CorpParty.LAST_NME.contains(query))) \
-            .paginate(int(page), 20, False)
+            )
+ 
+    if query:
+        results = results.filter((Corporation.CORP_NUM == query) | (CorpParty.FIRST_NME.contains(query)) | (CorpParty.LAST_NME.contains(query)))
+    elif grps:
+        filter_grp = reduce(
+            lambda accumulator, s: accumulator | _get_filter(*s),
+            grps[1:],
+            _get_filter(*grps[0])
+            )
+        results = results.filter(filter_grp)
+    else:
+        pass
+    results = results.paginate(int(page), 20, False)
 
     corp_parties = []
     for row in results.items:
