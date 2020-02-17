c006e1c316100029ea750130947eb1d72330addb
Denis Calixto <deniscalixto@gmail.com>
2020-02-16 16:47:49 -0800

    Exporting CorpParty search result as a xlsx file: install requirement, create tmp folder on build, create route for genereting the output and temp file.

--- search-api/app.py
+++ search-api/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, request, jsonify
+from openpyxl import Workbook
+from tempfile import NamedTemporaryFile
+
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from sqlalchemy import desc, func
 
@@ -229,6 +232,152 @@ def corpparty_search():
         corp_parties.append(result_dict)
     
     return jsonify({'results': corp_parties, 'total': total_results })
+
+
+@app.route('/person/search/export/')
+def corpparty_search_export():
+
+    args = request.args
+
+    page = int(args.get("page")) if "page" in args else 1
+
+    query = args.get("query")
+
+    fields = args.getlist('field')
+    operators = args.getlist('operator')
+    values = args.getlist('value')
+    mode = args.get('mode')
+    sort_type = args.get('sort_type')
+    sort_value = args.get('sort_value')
+
+    if query and len(fields) > 0:
+        raise Exception("use simple query or advanced. don't mix")
+    
+    # Only triples of clauses are allowed. So, the same number of fields, ops and values.
+    if len(fields) != len(operators) or len(operators) != len(values):
+        raise Exception("mismatched query param lengths: fields:{} operators:{} values:{}".format(
+            len(fields),
+            len(operators),
+            len(values)))
+
+    # Zip the lists, so ('LAST_NME', 'FIRST_NME') , ('contains', 'exact'), ('Sky', 'Apple') => (('LAST_NME', 'contains', 'Sky'), ('FIRST_NME', 'exact', 'Apple'))
+    clauses = list(zip(fields, operators, values))
+
+    # TODO: move queries to model class.
+    results = CorpParty.query\
+            .filter(CorpParty.END_EVENT_ID == None)\
+            .filter(CorpParty.PARTY_TYP_CD.in_(['FIO', 'DIR','OFF']))\
+            .filter(CorpName.END_EVENT_ID == None)\
+            .join(Corporation, Corporation.CORP_NUM == CorpParty.CORP_NUM)\
+            .join(CorpName, Corporation.CORP_NUM == CorpName.CORP_NUM)\
+            .join(Address, CorpParty.MAILING_ADDR_ID == Address.ADDR_ID)\
+            .add_columns(\
+                CorpParty.CORP_PARTY_ID, 
+                CorpParty.FIRST_NME, 
+                CorpParty.MIDDLE_NME,
+                CorpParty.LAST_NME,
+                CorpParty.APPOINTMENT_DT,
+                CorpParty.CESSATION_DT,
+                Corporation.CORP_NUM,
+                CorpName.CORP_NME,
+                Address.ADDR_LINE_1,
+                Address.POSTAL_CD,
+                Address.CITY,
+                Address.PROVINCE,
+            )
+    
+    # Simple mode - return reasonable results for a single search string:
+    if query:
+        results = results.filter((Corporation.CORP_NUM == query) | (CorpParty.FIRST_NME.contains(query)) | (CorpParty.LAST_NME.contains(query)))
+    # Advanced mode - return precise results for a set of clauses.
+    elif clauses:
+
+        # Determine if we will combine clauses with OR or AND. mode=ALL means we use AND. Default mode is OR
+        if mode == 'ALL':
+            def fn(accumulator, s):
+                return accumulator & _get_filter(*s)
+        else:
+            def fn(accumulator, s):
+                return accumulator | _get_filter(*s)
+        
+        # We use reduce here to join all the items in clauses with the & operator or the | operator.
+        # Similar to if we did "|".join(clause), but calling the boolean operator instead.
+        filter_grp = reduce(
+            fn,
+            clauses[1:],
+            _get_filter(*clauses[0])
+            )
+        results = results.filter(filter_grp)
+
+    # Sorting
+    if sort_type is None:
+        results = results.order_by(CorpParty.LAST_NME)
+    else:
+        field = _get_sort_field(sort_value)
+
+        if sort_type == 'desc':
+            results = results.order_by(desc(field))
+        else:
+            results = results.order_by(field)
+
+    # Exporting to Excel
+    wb = Workbook()
+
+    with NamedTemporaryFile(mode='w+b', dir='tmp', delete=True) as tmp:
+
+        sheet = wb.active
+
+        # Sheet headers (first row)
+        _ = sheet.cell(column=1, row=1, value="Person Id")
+        _ = sheet.cell(column=2, row=1, value="First Name")
+        _ = sheet.cell(column=3, row=1, value="Middle Name")
+        _ = sheet.cell(column=4, row=1, value="Last Name")
+        _ = sheet.cell(column=5, row=1, value="Appointment Date")
+        _ = sheet.cell(column=6, row=1, value="Cessation Date")
+        _ = sheet.cell(column=7, row=1, value="Corporation Id")
+        _ = sheet.cell(column=8, row=1, value="Corp Name")
+        _ = sheet.cell(column=9, row=1, value="Address")
+        _ = sheet.cell(column=10, row=1, value="Postal Code")
+        _ = sheet.cell(column=11, row=1, value="City")
+        _ = sheet.cell(column=12, row=1, value="Province")
+        
+        index = 2
+        for row in results:
+            
+            # CorpParty.CORP_PARTY_ID
+            _ = sheet.cell(column=1, row=index, value=row[1])
+            # CorpParty.FIRST_NME
+            _ = sheet.cell(column=2, row=index, value=row[2])
+            # CorpParty.MIDDLE_NME
+            _ = sheet.cell(column=3, row=index, value=row[3])
+            # CorpParty.LAST_NME
+            _ = sheet.cell(column=4, row=index, value=row[4])
+            # CorpParty.APPOINTMENT_DT
+            _ = sheet.cell(column=5, row=index, value=row[5])
+            # CorpParty.CESSATION_DT
+            _ = sheet.cell(column=6, row=index, value=row[6])
+            # Corporation.CORP_NUM
+            _ = sheet.cell(column=7, row=index, value=row[7])
+            # CorpName.CORP_NME
+            _ = sheet.cell(column=8, row=index, value=row[8])
+            # Address.ADDR_LINE_1
+            _ = sheet.cell(column=9, row=index, value=row[9])
+            # Address.POSTAL_CD
+            _ = sheet.cell(column=10, row=index, value=row[10])
+            # Address.CITY
+            _ = sheet.cell(column=11, row=index, value=row[11])
+            # Address.PROVINCE
+            _ = sheet.cell(column=12, row=index, value=row[12])
+            
+            index += 1
+
+        filename = "{}.{}".format(tmp.name,"xlsx")
+        wb.save(filename=filename)
+
+        # file name without the path
+        simple_name = filename.split('/')[len(filename.split('/'))-1]        
+
+        return send_from_directory('tmp',simple_name,as_attachment=True)
 
 
 @app.route('/person/<id>')
