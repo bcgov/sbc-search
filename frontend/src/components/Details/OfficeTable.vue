95c83b84d64a69bb37ee11a24e14c20b21a3ada7
Aaron Ahn <aaron@countable.ca>
2020-02-19 17:35:09 -0800

    pop 3 tables

--- frontend/src/components/Details/OfficeTable.vue
+++ frontend/src/components/Details/OfficeTable.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
+    <h5 class="font-weight-regular body-1 mb-3">
+      All Offices Held At {{ details.corp_nme }}
+    </h5>
     <div v-if="offices && offices.length > 0">
       <v-simple-table class="office-table">
         <template v-slot:default>
           <thead>
             <tr>
               <th>Office Held</th>
+              <th>Corp Party ID</th>
               <th>Year</th>
-              <th>Address</th>
             </tr>
           </thead>
           <tbody>
@@ -16,34 +19,133 @@
                 {{ o.short_desc }}
               </td>
               <td>
-                {{ o["year"] }}
+                <a
+                  :href="`#/details?corp_party_id=${o['corp_party_id']}`"
+                  target="blank"
+                  >{{ o["corp_party_id"] }}</a
+                >
               </td>
-              <td>{{ o["addr_line_1"] }}</td>
+              <td>
+                -
+              </td>
             </tr>
           </tbody>
         </template>
       </v-simple-table>
     </div>
+    <div v-else>
+      <h5>No offices found</h5>
+    </div>
+    <h5 class="font-weight-regular body-1 mb-3 mt-6">
+      Other {{ details.last_nme }}, {{ details.first_nme }}
+      {{ details.middle_nme }} at {{ details.corp_nme }}
+    </h5>
+    <div v-if="offices && offices.length > 0">
+      <v-simple-table class="office-table">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th>Office Held</th>
+              <th>Corp Party ID</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(o, index) in officesheld.same_addr" :key="index">
+              <td>
+                {{ o.party_typ_cd }}
+              </td>
+              <td>
+                <a
+                  :href="`#/details?corp_party_id=${o['corp_party_id']}`"
+                  target="blank"
+                  >{{ o["corp_party_id"] }}</a
+                >
+              </td>
+              <td>
+                {{ o.year || "-" }}
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </div>
+    <div v-else>
+      <h5>None found</h5>
+    </div>
+
+    <h5 class="font-weight-regular body-1 mb-3 mt-6">
+      Other {{ details.last_nme }}, {{ details.first_nme }}
+      {{ details.middle_nme }} at {{ details.addr }}
+    </h5>
+    <div v-if="offices && offices.length > 0">
+      <v-simple-table class="office-table">
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th>Office Held</th>
+              <th>Corp Party ID</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(o, index) in officesheld.same_name_and_company"
+              :key="index"
+            >
+              <td>
+                {{ o.party_typ_cd }}
+              </td>
+              <td>
+                <a
+                  :href="`#/details?corp_party_id=${o['corp_party_id']}`"
+                  target="blank"
+                  >{{ o["corp_party_id"] }}</a
+                >
+              </td>
+              <td>
+                {{ o.year || "-" }}
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </div>
+    <div v-else>
+      <h5>None found</h5>
+    </div>
   </div>
 </template>
 
 <script>
-import dayjs from "dayjs";
-
 export default {
   props: {
     officesheld: {
-      default: null,
+      default: function() {
+        return {
+          same_addr: null,
+          same_name_and_company: null
+        };
+      },
+      type: Object
+    },
+    details: {
+      default: function() {
+        return {
+          corp_nme: null,
+          last_nme: null,
+          first_nme: null,
+          middle_nme: null,
+          addr: null
+        };
+      },
       type: Object
     }
   },
   computed: {
     offices() {
       if (this.officesheld && this.officesheld.offices) {
-        return this.officesheld.offices.map(o => {
-          o.year = dayjs(o["appointment_dt"]).format("YYYY");
-          return o;
-        });
+        return this.officesheld.offices;
       } else {
         return null;
       }
