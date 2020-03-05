f405fd97f35d256cef750397e132d48d68097903
Shirley Yin <shirley@countable.ca>
2020-03-04 21:28:06 -0800

    update page titles and favicon

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -8,30 +8,49 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      title: 'Director Search',
+    }
   },
   {
     path: "/corpparty/:id",
     name: "corpPartyDetails",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/CorpPartyDetails.vue")
+      import(/* webpackChunkName: "about" */ "../views/CorpPartyDetails.vue"),
+    meta: {
+      title: (route) => `Details for Filing #${route.params.id}`
+    }
   },
   {
     path: "/corporation/:id",
     name: "corporationDetails",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/CorporationDetails.vue")
+      import(/* webpackChunkName: "about" */ "../views/CorporationDetails.vue"),
+    meta: {
+      title: (route) => `Corporation Details for Inc. #${route.params.id}`
+    }
   },
   {
     path: "/corporation",
     name: "corporation",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/CorporationSearch.vue")
+      import(/* webpackChunkName: "about" */ "../views/CorporationSearch.vue"),
+    meta: {
+      title: (route) => `Corporation Search`
+    }
   }
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+
+});
+
+router.beforeEach((to, from, next) => {
+    const { title } = to.meta;
+    document.title = typeof title === 'function' ? title(to) : title;
+    next();
 });
 
 export default router;
