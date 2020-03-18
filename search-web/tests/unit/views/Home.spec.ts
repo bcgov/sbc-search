09cc66d0e8c25b41f58601f5663aa3f1b7321728
Aaron Ahn <aaron@countable.ca>
2020-03-17 21:22:28 -0700

    passing intial hello world test

--- search-web/tests/unit/views/Home.spec.ts
+++ search-web/tests/unit/views/Home.spec.ts
@@ -1,10 +1,39 @@
-import { shallowMount } from "@vue/test-utils"
+import { shallowMount, createLocalVue } from "@vue/test-utils"
 import Home from "@/views/Home.vue"
+import store from '@/store/index'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
+
+Vue.use(Vuetify)
 
 
-describe('Foo', () => {
+
+
+
+
+
+describe('Home.vue', () => {
+
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+
+  let vuetify = new Vuetify({})
+  const $route = {
+    query: {}
+  }
+
+
   it('renders a div', () => {
-    const wrapper = shallowMount(Home)
+
+    const wrapper = shallowMount(Home, {
+      store,
+      mocks: {
+        $route,
+      },
+      localVue,
+      vuetify
+    })
     expect(wrapper.contains('div')).toBe(true)
   })
 })
