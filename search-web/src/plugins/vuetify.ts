9fc326dece822535d2e6f5db1a6eecc8111f06b5
Shirley Yin <shirley@countable.ca>
2020-03-24 11:25:11 -0700

    load vuetify css

--- search-web/src/plugins/vuetify.ts
+++ search-web/src/plugins/vuetify.ts
@@ -1,6 +1,8 @@
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import 'vuetify/dist/vuetify.min.css'
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
