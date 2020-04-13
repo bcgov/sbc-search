235f79dc5aacef92c1f38028e3cfcdeba9ff6c31
Aaron Ahn <aaron@countable.ca>
2020-04-13 12:56:30 -0700

    ui updates and try without mincss

--- search-web/src/plugins/vuetify.ts
+++ search-web/src/plugins/vuetify.ts
@@ -1,7 +1,7 @@
+// import "vuetify/dist/vuetify.min.css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify);
 
