4109fae9a5d7f0f4f5015391096feb9e6f662248
Aaron Ahn <aaron@countable.ca>
2020-04-20 16:22:57 -0700

    404 and mockdata working

---
+++ search-web/src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex justify-center align-center flex-column">
+    <h2 class="mb-10">Page Not Found</h2>
+    <v-btn
+      to="/"
+      class="d-blockfont-weight-bold elevation-0 font-16"
+      color="primary"
+      >Go To Home</v-btn
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
