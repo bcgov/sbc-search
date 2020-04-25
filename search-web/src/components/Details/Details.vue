de9151362432228e38e4df5f52541ad88e8a8678
Clark <clark@countable.ca>
2020-04-24 23:07:22 -0700

    Party type description

--- search-web/src/components/Details/Details.vue
+++ search-web/src/components/Details/Details.vue
@@ -149,7 +149,7 @@ export default {
         "deliveryAddr",
         "mailingAddr",
         "corpPartyEmail",
-        "partyTypCd",
+        "partyTypeDesc",
         "appointmentDt",
         "cessationDt",
         "stateTypCd",
@@ -185,7 +185,7 @@ export default {
         "deliveryAddr",
         "mailingAddr",
         "corpPartyEmail",
-        "partyTypCd",
+        "partyTypeDesc",
         "appointmentDt",
         "cessationDt"
       ]);
